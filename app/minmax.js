e6fe600ffe8d4011732758c602cad072b64d66ba
chinedu ofor <oforchinedu12@gmail.com>
2017-01-07 22:20:20 +0100

    Update on findMinMax function

--- app/minmax.js
+++ app/minmax.js
@@ -1,16 +1,22 @@
 module.exports = {
-	findMinMax: function(num){
-		var Min = Math.min.apply(null, num);
-		var Max = Math.max.apply(null, num);
-		var MinNMax = [Min,Max]
-		var	equal = []
-		
-		for(i= 0; i < num.length; i++){
-			if(num[i] == num[i+1]){
-				 equal.push(num[i])
-				return equal
-			}
-			else return MinNMax
+	findMinMax: function(arr){
+		var min = arr[0];
+		var max = arr[0];
+
+		for(var i = 1; i < arr.length; i++){
+		    if(arr[i] < min){
+		      min = arr[i];
+		    }
+		    if(arr[i] > max){
+		      max = arr[i];
+		    }
+		}
+
+		if(min == max){
+			return [min];
+		}
+		else{
+			return [min,max];
 		}
 	}
 }
